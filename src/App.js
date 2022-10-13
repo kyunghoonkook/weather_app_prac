3cbb82b84dfd60adc2ccd857674500d3a224c7e3
kyunghoonkook <rnrrudgg68@gmail.com>
2022-10-13 19:45:51 +0900

    weather app prac_v0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import WeatherBox from "./components/WeatherBox";
+import WeatherButton from "./components/WeatherButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [apiError, setAPIError] = useState("");
+  const cities = ["paris", "new york", "tokyo", "seoul"];
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4dab3feed643eb675ee3df9bdf7306c8&units=metric`;
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+
+  const getWeaherByCity = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4dab3feed643eb675ee3df9bdf7306c8&units=metric`;
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (city === null) {
+      setLoading(true);
+      getCurrentLocation();
+    } else {
+      setLoading(true);
+      getWeaherByCity();
+    }
+  }, [city]);
+
+  //useEffect가 바뀔때 1. ui 그려지고 ,2. 배열에있는 state값이 바뀔때/// 상황에 맞춰서 호출가
+
+  const handleCityChange = (city) => {
+    if (city === "current") {
+      setCity(null);
+    } else {
+      setCity(city);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <div className="container">
+          <ClipLoader
+            color="#f88c6b"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : apiError ? (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton
+            cities={cities}
+            handleCityChange={handleCityChange}
+            selectedCity={city}
+          />
+        </div>
+      ) : (
+        apiError
+      )}
     </div>
   );
 }
